3aef6db3694c583bde319549cddb2f7e99430587
Ker Ser Khim <vandzaxe@yahoo.com>
2022-12-09 19:40:03 +0800

    Added filemgr

---
+++ filemgr/filemgr.go
@@ -0,0 +1,67 @@
+package filemgr
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"os"
+)
+
+// Return true if file does not exist.
+func FileNotExist(filename string) (bool, error) {
+	_, err := os.Stat(filename)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return true, nil
+		}
+
+		return false, err // There's other error.
+	}
+
+	return false, nil // File exist.
+}
+
+// Create file if file does not exist.
+func CreateFileIfNotExist(filename string, content string) error {
+	notExist, err := FileNotExist(filename)
+	if err != nil {
+		return errors.New(fmt.Sprintf("Error checking if file exist: %s", err.Error()))
+	}
+
+	if notExist {
+		file, err := os.Create(filename)
+		if err != nil {
+			return errors.New(fmt.Sprintf("Error creating file: %s", err.Error()))
+		}
+		defer file.Close()
+
+		if content != "" {
+			_, err := file.WriteString(content)
+			if err != nil {
+				return errors.New(fmt.Sprintf("Error writing to file: %s", err.Error()))
+			}
+		}
+	}
+
+	return nil
+}
+
+// Delete file if exist.
+func DeleteFileIfExist(filename string) error {
+	notExist, err := FileNotExist(filename)
+	if err != nil {
+		return errors.New(fmt.Sprintf("Error checking whether file exist: %s", err.Error()))
+	}
+
+	if notExist {
+		log.Println("File does not exist:", filename)
+	} else {
+		err := os.Remove(filename) // Delete file.
+		if err != nil {
+			return errors.New(fmt.Sprintf("Error deleting file: %s", err.Error()))
+		}
+		log.Println("File exists and deleted:", filename)
+	}
+
+	return nil
+}
--- helpr_test.go
+++ helpr_test.go
@@ -3,6 +3,7 @@ package helpr
 import (
 	"testing"
 
+	"github.com/skker/helpr/filemgr"
 	"github.com/skker/helpr/str"
 	"github.com/stretchr/testify/assert"
 )
@@ -19,4 +20,34 @@ func TestStr(t *testing.T) {
 	substr = "land"
 	result = str.CaseInsensitiveSearch(s, substr)
 	assert.False(t, result, "String ("+s+") should not contain "+substr)
+}
+
+func TestFilemgr(t *testing.T) {
+	t.Log("\n\nTest str package...")
+
+	filename := "test.txt"
+	content := "test test only..."
+	err := filemgr.CreateFileIfNotExist(filename, content)
+	if err != nil {
+		t.Fatal("Error calling CreateFileIfNotExist")
+	}
+
+	notExist, err := filemgr.FileNotExist(filename)
+	if err != nil {
+		t.Fatal("Error calling FileNotExist")
+	}
+
+	assert.Equal(t, false, notExist, "FileNotExist should return false after creating file")
+
+	err = filemgr.DeleteFileIfExist(filename)
+	if err != nil {
+		t.Fatal("Error calling DeleteFileIfExist")
+	}
+
+	notExist, err = filemgr.FileNotExist(filename)
+	if err != nil {
+		t.Fatal("Error calling FileNotExist")
+	}
+
+	assert.Equal(t, true, notExist, "FileNotExist should return true after calling DeleteFileIfExist")
 }
